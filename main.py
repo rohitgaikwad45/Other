ba18a772811f8b921a1bacee7c752ddaead96781
Rohit Gaikwad <119856817+rohitgaikwad5858@users.noreply.github.com>
2024-08-09 18:32:29 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-
-st.write(f'Forecast plot for {n_years} years')
-fig1 = plot_plotly(m, forecast)
-st.plotly_chart(fig1)
-
-st.write("Forecast components")
-fig2 = m.plot_components(forecast)
-st.write(fig2)
-
